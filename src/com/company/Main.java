49ed501013bc9f001b905f8d383d7db4a13c4477
Maria Wiik <wiikcandido.maria@syk.fi>
2022-08-29 20:15:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        boolean a = true, b = false, statement;
+        if (a == b)
+            statement = true;
+        else
+            statement = false;
+        System.out.println(statement);
+    }
+}
